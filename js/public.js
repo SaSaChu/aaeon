d706c9250ac8a5fb1ccdea8a3258fcf80d8e3561
SaSaChu <teresa7397@gmail.com>
2016-05-13 00:17:38 +0800

    完成 year tabs.

--- js/public.js
+++ js/public.js
@@ -4,5 +4,23 @@
  */
 
 $(function () {
+  $('#year_tab').each (function () {
 
+    var $a = $(this).find ('> a');
+    var $c = $(this).find ('> div > div');
+
+    $(this).data ('i', $c.find ('a.a').index ());
+    $c.attr ('class', 'p' + $c.find ('a.a').index ());
+
+    $a.first ().click (function () {
+      if ($(this).data ('i') + 1 < $c.find ('a').length)
+        $(this).data ('i', $(this).data ('i') + 1);
+      $c.attr ('class', 'p' + $(this).data ('i'));
+    }.bind ($(this)));
+    $a.last ().click (function () {
+      if ($(this).data ('i') > 0)
+        $(this).data ('i', $(this).data ('i') - 1);
+      $c.attr ('class', 'p' + $(this).data ('i'));
+    }.bind ($(this)));
+  });
 });
