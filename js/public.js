73dde2b34a6afbde344ecd4fbebd4da81feda81d
SaSaChu <teresa7397@gmail.com>
2016-05-19 23:18:38 +0800

    no message

--- js/public.js
+++ js/public.js
@@ -4,6 +4,18 @@
  */
 
 $(function () {
+  $('.login, .lbox-login .cover, .lbox-login .close').click (function () {
+    $('.lbox-login').toggleClass ('s');
+  });
+  $('.lbox-forget .cover, .lbox-forget .close').click (function () {
+    $('.lbox-forget').toggleClass ('s');
+  });
+
+  $('.forget').click (function () {
+    $('.lbox-login').toggleClass ('s');
+    $('.lbox-forget').toggleClass ('s');
+  });
+
 
   if ($(window).width () < 960) {
     $('.lb').click (function () {
