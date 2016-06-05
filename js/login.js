d885065e42a861c0f9c3b6e9c145c794d0bb10af
OA Wu <comdan66@gmail.com>
2016-06-05 11:55:39 +0800

    Temp

---
+++ js/login.js
@@ -0,0 +1,12 @@
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2016 OA Wu Design
+ */
+
+$(function () {
+  var $loginBox = $('#login-box');
+
+  $('#login').click (function () {
+    $loginBox.addClass ('show');
+  });
+});
